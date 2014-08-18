838b097878ea7dd80abf6af913cc9a59312561be
/users/rashmi/Documents <rashmi@aryamac2.local>
2014-08-18 10:21:33 -0700

    Initial Commit

---
+++ Mr. Park/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mr. Park
+//
+//  Created by rashmi on 8/18/14.
+//  Copyright (c) 2014 arya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
